2c9590be11b5dfd166485ba962903ff16469b053
Abhishek Nilesh Joshi <abhishek.joshi@perkinelmer.com>
2023-02-12 22:05:26 +0530

    update wl count

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,2 +1,2 @@
 let scLink =
-	'https://script.google.com/macros/s/AKfycbxmTk3q0Wf7yLoXJmJDopZqYTx71QsHTS-J2iUIl6Iq5ovfZzOOLhZjQ6CsfuKXgakO/exec';
+	'https://script.google.com/macros/s/AKfycbzq38TbjdmnAp-BSIA9NbpEYepqWcCR1BNmbd1hZLrSOxb9NK-sKAjrHTV2A01bvUBA/exec';
