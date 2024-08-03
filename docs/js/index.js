d02435b3dbb68965a243e74beb4be1ce8b80fe16
Abhishek Nilesh Joshi <abhishek.joshi@perkinelmer.com>
2024-08-03 22:47:39 +0530

    .

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,2 +1,2 @@
 let scLink =
-	'https://script.google.com/macros/s/AKfycbx3fVXnVQGEm8zUMnLN8FNromDo4R35ARpgXfk0_KgoEHVJqBIgcI5UC2esSxd5GhTn/exec';
+	'https://script.google.com/macros/s/AKfycbzMQx0B7IN835VZBOMGzCCPyfY0OZKs-qry-EU4iqpnOpMClaoETp_kUChP326j5Z5X/exec';
